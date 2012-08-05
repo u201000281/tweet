e76321eaa3377b00bc49e91e5d55468476955c85
Alfredo Mendieta <u201000281@gmail.com>
2012-08-05 18:01:34 -0500

    app tweet

---
+++ appTweet/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AppTweet::Application.config.secret_token = '3085792cb2f8d9c285d9a34010dde6f5e823e3b433c8f2c3a13b587530fbcbdd62d500951d56bee135b6f5f2b0978ecc1a6431dfdf8eabbf0a02728d48ba5319'
